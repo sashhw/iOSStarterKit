67f795a7e426575e3b8394e02f170f240e3f014c
swalkowski <swalkowski@foxtrotco.com>
2024-02-21 11:00:49 -0600

    Initial Commit

---
+++ iOS Starter Kit/iOS_Starter_KitApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Starter_KitApp.swift
+//  iOS Starter Kit
+//
+//  Created by Sasha Walkowski on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Starter_KitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
